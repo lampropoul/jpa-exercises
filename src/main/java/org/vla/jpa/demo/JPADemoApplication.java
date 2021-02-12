9c67fab817c44c22595eadaa22585fb88f549cfb
Vasileios Lampropoulos <vla@anixe.io>
2021-02-12 14:21:21 +0200

    Initial commit

---
+++ src/main/java/org/vla/jpa/demo/JPADemoApplication.java
@@ -0,0 +1,13 @@
+package org.vla.jpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPADemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JPADemoApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/vla/jpa/demo/JPADemoApplicationTests.java
@@ -0,0 +1,14 @@
+package org.vla.jpa.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JPADemoApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
